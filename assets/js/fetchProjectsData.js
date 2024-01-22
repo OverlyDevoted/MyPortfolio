04c82845725fe76520286e747c8f7fb9a764e94d
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2024-01-22 20:12:09 +0200

    Data Fetch fix

--- assets/js/fetchProjectsData.js
+++ assets/js/fetchProjectsData.js
@@ -1,3 +1,5 @@
-const response = await fetch(`${window.origin}/assets/data/projects.json`)
+let origin = window.origin;
+let isLocalHost = origin.includes("localhost") || origin.includes("127.0.0.1")
+const response = await fetch(`${origin}${isLocalHost ? "" : "/MyPortfolio"}/assets/data/projects.json`)
 export const projects = await response.json()
 projects.sort((a, b) => a.id - b.id)
