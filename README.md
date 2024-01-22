8e3d1804a62401d168a08a55005b3fe4e2561388
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2024-01-22 13:31:36 +0200

    Links and languages

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Portfolio page
 
 This is a repository for my vanilla JavaScript portfolio page.
+
+## To do
+
+- [ ] Post styling
+- [ ] On hover increase website photo size
+- [ ] Button for cycling between posts
+- [ ] Post cycle animation:
+      - Post pans to left and goes transparent
+      - Another post pans from the right and goes opaque 
+      - Main element background color changes
--- assets/js/projectPost.js
+++ assets/js/projectPost.js
@@ -4,25 +4,62 @@ export default class ProjectHandler {
         const postContainer = document.createElement("div");
         parent.replaceChild(postContainer, document.getElementById("loading-container"))
         postContainer.classList.add("post-container")
-        
+
         // create post elements
         this.title = document.createElement("h2");
         this.description = document.createElement("p");
         this.icon = document.createElement("img");
 
         this.desktop = document.createElement("img");
-        this.desktop.classList.add("preview-desk")
+        this.desktop.classList.add("preview-desk");
 
-        postContainer.append(this.title, this.description, this.desktop)
+        const additionalInf = document.createElement("div");
+        additionalInf.classList.add("additional-info");
+
+        this.languages = document.createElement("div");
+        this.languages.classList.add("languages");
+
+        const links = document.createElement("div");
+        links.classList.add("links");
+
+        this.repo = this.createLink("Repository")
+        this.project = this.createLink("Live Project")
+
+        links.append(this.repo, this.project)
+
+        additionalInf.append(links, this.languages);
+        postContainer.append(this.title, this.desktop, additionalInf, this.description);
 
         //loading data
         this.loadPost(initPost);
+    }
+    createLink(name) {
+        const aTag = document.createElement("a");
+        aTag.textContent = name;
+        aTag.setAttribute("target", "_blank");
+        aTag.setAttribute("referrerpolicy", "no-referrer");
+        return aTag;
+    }
+    createTagLanguage(language) {
+        const element = document.createElement("span");
+        element.classList.add(language.replace(/[$&+,:;=?@#|'<>.^*()%!-]/g,"") + "-lang");
+        element.textContent = language;
+        return element;
     }
     loadPost(post) {
         this.title.textContent = post.title;
         this.description.textContent = post.description;
         this.icon.src = post.icon;
         this.desktop.src = post.desktop;
+
+        this.repo.href = post.repo_url;
+        this.project.href = post.url;
+
+        this.languages.innerHTML = ""
+        post.languages.forEach((language) => {
+            this.languages.appendChild(this.createTagLanguage(language.toLowerCase()));
+        })
+
     }
     //so that we could change fill color
     async loadInlineIcon(link) {
