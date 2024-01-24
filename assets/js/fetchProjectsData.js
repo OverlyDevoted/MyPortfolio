14edc8d1f64872d8e1c5be478d140ede2195d31e
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2024-01-24 09:08:20 +0200

    Domain fix

--- assets/js/fetchProjectsData.js
+++ assets/js/fetchProjectsData.js
@@ -1,6 +1,5 @@
 let origin = window.origin;
-let isLocalHost = origin.includes("localhost") || origin.includes("127.0.0.1")
-const response = await fetch(`${origin}${isLocalHost ? "" : "/MyPortfolio"}/assets/data/projects.json`)
+const response = await fetch(`${origin}/assets/data/projects.json`)
 const promise = await new Promise((resolve, reject) => setTimeout(() => { resolve() }, 1000))
 
 export const projects = await response.json()
