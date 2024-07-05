6e47935a95763d01a6d83b87b1355badc600ee64
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2024-07-05 22:51:19 +0300

    Dates for projects and new projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,51 @@
 const arrowsAppear = 2000;
 const arrowsBreathe = 2000;
 
-const { projects } = await import("./fetchProjectsData.js")
+const { projects } = await import("./fetchProjectsData.js");
 
-import ProjectHandler from "./projectPost.js"
+import ProjectHandler from "./projectPost.js";
 
-const projectsHandler = new ProjectHandler(document.getElementById("main"), projects)
-const nextBtn = document.getElementById("nextBtn")
-const prevBtn = document.getElementById("prevBtn")
+const projectsHandler = new ProjectHandler(
+  document.getElementById("main"),
+  projects
+);
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
 const shadow = document.getElementById("mouse-shadow");
 let breatheInterval;
 let breatheTimeout;
 const registerEvents = (btn, btn2, side = "right") => {
-    btn.addEventListener("click", () => {
-        projectsHandler.startLoad(side)
-    })
-    btn.addEventListener("mouseenter", () => {
-        clearInterval(breatheInterval);
-        clearTimeout(breatheTimeout);
-        btn.classList.add("post-btn")
-        shadow.style.setProperty("--shadow-opacity", "1")
-        shadow.style.left = btn.textContent === "<" ? "0" : "100%";
-        // btn2.classList.add("post-btn")
-    })
-    btn.addEventListener("mouseleave", () => {
-        btn.classList.remove("post-btn")
-        btn2.classList.remove("post-btn")
+  btn.addEventListener("click", () => {
+    projectsHandler.startLoad(side);
+  });
+  btn.addEventListener("mouseenter", () => {
+    clearInterval(breatheInterval);
+    clearTimeout(breatheTimeout);
+    btn.classList.add("post-btn");
+    shadow.style.setProperty("--shadow-opacity", "1");
+    shadow.style.left = btn.textContent === "<" ? "0" : "100%";
+  });
+  btn.addEventListener("mouseleave", () => {
+    btn.classList.remove("post-btn");
+    btn2.classList.remove("post-btn");
 
-        shadow.style.setProperty("--shadow-opacity", "0")
-        breatheTimeout = setTimeout(() => {
-            breatheInterval = setInterval(() => {
-                btn.classList.toggle("post-btn")
-                btn2.classList.toggle("post-btn")
-            }, arrowsBreathe);
-        }, arrowsAppear);
-    })
-    btn.addEventListener("mousemove", (e) => {
-        shadow.style.transform = `translateY(${e.clientY}px)`;
-    })
-}
+    shadow.style.setProperty("--shadow-opacity", "0");
+    breatheTimeout = setTimeout(() => {
+      breatheInterval = setInterval(() => {
+        btn.classList.toggle("post-btn");
+        btn2.classList.toggle("post-btn");
+      }, arrowsBreathe);
+    }, arrowsAppear);
+  });
+  btn.addEventListener("mousemove", (e) => {
+    shadow.style.transform = `translateY(${e.clientY}px)`;
+  });
+};
 breatheTimeout = setTimeout(() => {
-    breatheInterval = setInterval(() => {
-        nextBtn.classList.toggle("post-btn")
-        prevBtn.classList.toggle("post-btn")
-    }, arrowsBreathe);
+  breatheInterval = setInterval(() => {
+    nextBtn.classList.toggle("post-btn");
+    prevBtn.classList.toggle("post-btn");
+  }, arrowsBreathe);
 }, arrowsAppear);
-registerEvents(nextBtn, prevBtn)
-registerEvents(prevBtn, nextBtn, "left")
+registerEvents(nextBtn, prevBtn);
+registerEvents(prevBtn, nextBtn, "left");
--- assets/js/projectPost.js
+++ assets/js/projectPost.js
@@ -1,129 +1,140 @@
+import { months } from "./constants/months.js";
+
 export default class ProjectHandler {
-    constructor(parent, projects) {
-        // setup initial HTML
-        this.postContainer = document.createElement("article");
-        parent.replaceChild(this.postContainer, document.getElementById("loading-container"))
-        this.postContainer.classList.add("post-container")
-        this.postContainer.classList.add("first")
-        this.postContainer.id = "post"
-        setTimeout(() => {
-            this.postContainer.classList.remove("first")
-        }, 1);
+  constructor(parent, projects) {
+    // setup initial HTML
+    this.postContainer = document.createElement("article");
+    parent.replaceChild(
+      this.postContainer,
+      document.getElementById("loading-container")
+    );
+    this.postContainer.classList.add("post-container");
+    this.postContainer.classList.add("first");
+    this.postContainer.id = "post";
+    setTimeout(() => {
+      this.postContainer.classList.remove("first");
+    }, 1);
 
-        // create post elements
-        this.title = document.createElement("h2");
-        this.description = document.createElement("p");
-        this.icon = document.createElement("img");
+    // create post elements
+    this.title = document.createElement("h2");
+    this.date = document.createElement("h3");
+    this.description = document.createElement("p");
+    this.icon = document.createElement("img");
 
-        const previewDiv = document.createElement("section")
-        previewDiv.classList.add("preview-container")
-        this.desktop = document.createElement("img");
-        this.desktop.classList.add("preview-desk");
-        previewDiv.append(this.desktop)
+    const previewDiv = document.createElement("section");
+    previewDiv.classList.add("preview-container");
+    this.desktop = document.createElement("img");
+    this.desktop.classList.add("preview-desk");
+    previewDiv.append(this.desktop);
 
-        const info = document.createElement("section")
-        const additionalInf = document.createElement("div");
-        additionalInf.classList.add("additional-info");
+    const info = document.createElement("section");
+    const additionalInf = document.createElement("div");
+    additionalInf.classList.add("additional-info");
 
-        this.languages = document.createElement("div");
-        this.languages.classList.add("languages");
+    this.languages = document.createElement("div");
+    this.languages.classList.add("languages");
 
-        this.links = document.createElement("div");
-        this.links.classList.add("links");
+    this.links = document.createElement("div");
+    this.links.classList.add("links");
 
-        additionalInf.append(this.links, this.languages);
-        info.append(additionalInf, this.description)
+    additionalInf.append(this.links, this.languages);
+    info.append(additionalInf, this.description);
 
-        this.postContainer.append(this.title, previewDiv, info);
+    this.postContainer.append(this.title, this.date, previewDiv, info);
 
-        //other variables
-        this.projects = projects
-        this.curPost = 1;
+    //other variables
+    this.projects = projects;
+    this.curPost = 1;
 
-        //loading data
-        this.loadPost();
-        this.side = undefined;
-        this.loading = false;
-        this.root = document.querySelector(":root");
-        this.counter = 0;
-        document.getElementById("post").addEventListener("transitionend", async (e) => {
-            if (e.target.classList.contains("default-pos")) {
-                this.loading = false;
-                e.target.classList.remove("default-pos")
-                return;
-            }
-            if (!e.target.classList.contains(`${this.side}-fade-out`))
-                return;
-            this.root.style.setProperty("--fade-in-timer", "unset")
-            e.target.classList.add(`${this.side}-fade-in`)
-            const promise = () => new Promise((res) => {
-                setTimeout(() => {
-                    res();
-                }, 0);
-            })
-            await promise()
-            if (!e.target.classList.contains("default-pos"))
-                this.loadPost()
-
-            this.root.style.setProperty("--fade-in-timer", getComputedStyle(this.root).getPropertyValue("--default-fade-in-timer"))
-            e.target.classList.remove(`${this.side}-fade-in`)
-            e.target.classList.remove(`${this.side}-fade-out`)
-            e.target.classList.add("default-pos")
-        })
-    }
-    createLink(link) {
-        const aTag = document.createElement("a");
-        aTag.textContent = link.text;
-        aTag.setAttribute("href", link.url);
-        aTag.setAttribute("target", "_blank");
-        aTag.setAttribute("referrerpolicy", "no-referrer");
-        return aTag;
-    }
-    createTagLanguage(language) {
-        const element = document.createElement("span");
-        element.classList.add(language.replace(/[$&+,:;=?@#|'<>.^*()%!-]/g, "") + "-lang");
-        element.textContent = language;
-        return element;
-    }
-    startLoad(side) {
-        if (!this.loading) {
-            this.side = side;
-            this.postContainer.classList.add(`${this.side}-fade-out`)
-
-            this.loading = true;
+    //loading data
+    this.loadPost();
+    this.side = undefined;
+    this.loading = false;
+    this.root = document.querySelector(":root");
+    this.counter = 0;
+    document
+      .getElementById("post")
+      .addEventListener("transitionend", async (e) => {
+        if (e.target.classList.contains("default-pos")) {
+          this.loading = false;
+          e.target.classList.remove("default-pos");
+          return;
         }
+        if (!e.target.classList.contains(`${this.side}-fade-out`)) return;
+        this.root.style.setProperty("--fade-in-timer", "unset");
+        e.target.classList.add(`${this.side}-fade-in`);
+        const promise = () =>
+          new Promise((res) => {
+            setTimeout(() => {
+              res();
+            }, 0);
+          });
+        await promise();
+        if (!e.target.classList.contains("default-pos")) this.loadPost();
+
+        this.root.style.setProperty(
+          "--fade-in-timer",
+          getComputedStyle(this.root).getPropertyValue(
+            "--default-fade-in-timer"
+          )
+        );
+        e.target.classList.remove(`${this.side}-fade-in`);
+        e.target.classList.remove(`${this.side}-fade-out`);
+        e.target.classList.add("default-pos");
+      });
+  }
+
+  createLink(link) {
+    const aTag = document.createElement("a");
+    aTag.textContent = link.text;
+    aTag.setAttribute("href", link.url);
+    aTag.setAttribute("target", "_blank");
+    aTag.setAttribute("referrerpolicy", "no-referrer");
+    return aTag;
+  }
+
+  createTagLanguage(language) {
+    const element = document.createElement("span");
+    const langFormatted = language.toLowerCase();
+    element.classList.add(
+      langFormatted.replace(/[$&+,:;=?@#|'<>.^*()%!-]/g, "") + "-lang"
+    );
+    element.textContent = langFormatted;
+    return element;
+  }
+
+  startLoad(side) {
+    if (!this.loading) {
+      this.side = side;
+      this.postContainer.classList.add(`${this.side}-fade-out`);
+
+      this.loading = true;
     }
-    loadPost() {
-        const dec = this.side == "left" ? 1 : -1;
-        this.curPost = (this.projects.length + (this.curPost + dec)) % this.projects.length;
+  }
 
-        this.title.textContent = this.projects[this.curPost].title;
-        this.description.textContent = this.projects[this.curPost].description;
-        this.desktop.src = this.projects[this.curPost].desktop;
+  loadPost() {
+    const dec = this.side == "left" ? 1 : -1;
+    this.curPost =
+      (this.projects.length + (this.curPost + dec)) % this.projects.length;
 
-        // this.repo = this.createLink("Repository")
-        // this.project = this.createLink("Live Project")
-        // this.repo.href = this.projects[this.curPost].repo_url;
-        // if (this.projects[this.curPost].url) {
-        //     this.project.href = this.projects[this.curPost].url;
-        //     this.project.textContent = "Live Project"
-        // }
-        // else
-        //     this.project.textContent = ""
+    this.title.textContent = this.projects[this.curPost].title;
+    const postDate = new Date(this.projects[this.curPost].date * 1000);
+    const formatedDate = `${postDate.getFullYear()} ${
+      months[postDate.getUTCMonth()]
+    }`;
+    this.date.textContent = formatedDate;
+    this.description.textContent = this.projects[this.curPost].description;
+    this.desktop.src = this.projects[this.curPost].desktop;
 
-        this.links.innerHTML = "";
-        this.projects[this.curPost].links.map((link) => {
-            const linkElement = this.createLink(link);
-            this.links.append(linkElement);
-        })
+    this.links.innerHTML = "";
+    this.projects[this.curPost].links.map((link) => {
+      const linkElement = this.createLink(link);
+      this.links.append(linkElement);
+    });
 
-        this.languages.innerHTML = ""
-        this.projects[this.curPost].languages.forEach((language) => {
-            this.languages.appendChild(this.createTagLanguage(language.toLowerCase()));
-        })
-    }
-
-    //so that we could change fill color
-
+    this.languages.innerHTML = "";
+    this.projects[this.curPost].languages.forEach((language) => {
+      this.languages.appendChild(this.createTagLanguage(language));
+    });
+  }
 }
-
